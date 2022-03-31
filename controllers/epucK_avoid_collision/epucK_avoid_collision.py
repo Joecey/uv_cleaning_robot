0d6793a2da896b01012bc90722123c5ef1476e4b
Joe Linogao <linogaoj@tcd.ie>
2022-03-31 22:30:30 +0100

    completed tutorial 4

--- controllers/epucK_avoid_collision/epucK_avoid_collision.py
+++ controllers/epucK_avoid_collision/epucK_avoid_collision.py
@@ -27,6 +27,11 @@ left_motor.setVelocity(0.0)
 right_motor.setVelocity(0.0)
 
 
+# set max speed and wheel speeds
+MAX_SPEED = 6.28
+
+
+
 # open feedback loop (continue simulation until exit command recieved)
 while robot.step(TIME_STEP) != -1:
     # take in sensor data here
@@ -38,7 +43,30 @@ while robot.step(TIME_STEP) != -1:
         psValues.append(ps[i].getValue())
     
     # print distance values
-    print(psValues)
+    # print(psValues)
     
     # based on what robot sees do this 
-    #...
+    # determine bool state of left/right obstacles 
+    right_obstacle = psValues[0] > 80 or psValues[1] > 80 or psValues[2] > 80
+    left_obstacle = psValues[5] > 80 or psValues[6] > 80 or psValues[7] > 80
+    
+    # print right/left obstacle
+    print(right_obstacle, left_obstacle)
+    
+    # set wheel speed based on sensor readings
+    if left_obstacle:
+        left_speed = 0.5 * MAX_SPEED
+        right_speed = -0.5 * MAX_SPEED
+        
+        
+    elif right_obstacle:
+        left_speed = -0.5 * MAX_SPEED
+        right_speed = 0.5 * MAX_SPEED
+    
+    else:
+        left_speed = 0.5 * MAX_SPEED
+        right_speed = 0.5 * MAX_SPEED
+        
+    # write actuators inputs
+    left_motor.setVelocity(left_speed)
+    right_motor.setVelocity(right_speed)
